391072617bc5a4c0627bf7b8d337f0c5301ef58f
michioxd <michio.haiyaku@gmail.com>
2025-12-10 16:55:25 +0700

    feat: enhance SpaceAutoGenerator to properly generate replay frames based on hit object positions and timings

--- osu.Game.Rulesets.Space/Replays/SpaceAutoGenerator.cs
+++ osu.Game.Rulesets.Space/Replays/SpaceAutoGenerator.cs
@@ -3,6 +3,10 @@ using osu.Game.Rulesets.Space.Objects;
 using osu.Game.Rulesets.Replays;
 using System.Collections.Generic;
 using osu.Game.Rulesets.Mods;
+using osu.Framework.Utils;
+using osu.Framework.Graphics;
+using osuTK;
+using System;
 
 namespace osu.Game.Rulesets.Space.Replays
 {
@@ -17,14 +21,56 @@ namespace osu.Game.Rulesets.Space.Replays
 
         protected override void GenerateFrames()
         {
-            Frames.Add(new SpaceReplayFrame());
+            if (Beatmap.HitObjects.Count == 0)
+                return;
+
+            Frames.Add(new SpaceReplayFrame { Position = Beatmap.HitObjects[0].Position });
 
             foreach (SpaceHitObject hitObject in Beatmap.HitObjects)
             {
+                moveToHitObject(hitObject);
+
                 Frames.Add(new SpaceReplayFrame
                 {
                     Time = hitObject.StartTime,
                     Position = hitObject.Position,
+                });
+            }
+        }
+
+        private void moveToHitObject(SpaceHitObject h)
+        {
+            if (Frames.Count == 0) return;
+
+            SpaceReplayFrame lastFrame = (SpaceReplayFrame)Frames[Frames.Count - 1];
+            Vector2 startPosition = lastFrame.Position;
+            Vector2 endPosition = h.Position;
+
+            double reactionTime = 100;
+
+            double moveStartTime = h.StartTime - h.TimePreempt + reactionTime;
+
+            moveStartTime = Math.Max(moveStartTime, lastFrame.Time);
+            moveStartTime = Math.Min(moveStartTime, h.StartTime);
+
+            double frameInterval = 1000.0 / 60.0;
+
+            for (double t = lastFrame.Time + frameInterval; t < moveStartTime; t += frameInterval)
+            {
+                Frames.Add(new SpaceReplayFrame
+                {
+                    Time = t,
+                    Position = startPosition
+                });
+            }
+
+            for (double t = moveStartTime; t < h.StartTime; t += frameInterval)
+            {
+                Vector2 pos = Interpolation.ValueAt(t, startPosition, endPosition, moveStartTime, h.StartTime, Easing.Out);
+                Frames.Add(new SpaceReplayFrame
+                {
+                    Time = t,
+                    Position = pos
                 });
             }
         }
